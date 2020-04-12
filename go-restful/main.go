59a7069ac8b3789ef9dd83344271d0a79299174e
hunghh.dev <hunghh.dev@gmail.com>
2020-04-12 20:45:42 +0700

    add mux module

--- go-restful/main.go
+++ go-restful/main.go
@@ -5,17 +5,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-type Article struct{
-	Title string `json:"Title"`
-	Desc string `json:"desc"`
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
 	Content string `json:"content"`
 }
 
 type Articles []Article
 
-func allArticles(w http.ResponseWriter, r *http.Request){
+func allArticles(w http.ResponseWriter, r *http.Request) {
 	articles := Articles{
 		Article{Title: "test title", Desc: "test dest", Content: "test content"},
 	}
@@ -24,14 +26,17 @@ func allArticles(w http.ResponseWriter, r *http.Request){
 	json.NewEncoder(w).Encode(articles)
 }
 
-func homePage(w http.ResponseWriter, r *http.Request)  {
+func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Homepage Endpoint Hit")
 }
 
-func handleRequests()  {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/articles", allArticles)
-	log.Fatal(http.ListenAndServe(":1025", nil))
+func handleRequests() {
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", allArticles).Methods("GET")
+	log.Fatal(http.ListenAndServe(":1025", myRouter))
 }
 
 func main() {
