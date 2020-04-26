a0c479dcc7b88df2fa26fe30e2eb2cd6ba01b5f4
Hunghh.Dev <hunghh.dev@gmail.com>
2020-04-26 17:48:40 +0700

    [leet-code] decompress run length encoded list

--- leet-code/decompress-run-length-encoded-list/main.go
+++ leet-code/decompress-run-length-encoded-list/main.go
@@ -3,16 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	nums := [...]int{1,2,3,4}
+	nums := [...]int{1, 2, 3, 4}
 	fmt.Println(decompressRLElist(nums[:]))
 }
 
 func decompressRLElist(nums []int) []int {
 	var s []int
-    for i := 0;  i<len(nums); i=i+2 {
-		fmt.Println(i)
-		for j := 0; j<nums[i]; j++{
-			fmt.Println(j)
+	for i := 0; i < len(nums); i = i + 2 {
+		for j := 0; j < nums[i]; j++ {
 			s = append(s, nums[i+1])
 		}
 	}
