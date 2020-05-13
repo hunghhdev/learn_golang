d6e180668248a6283d7f1350053a80ecce94e5f0
Hunghh.Dev <hunghh.dev@gmail.com>
2020-05-13 16:58:40 +0700

    [go-microservices] Update rows

--- go-microservices/bookstore_users-api/src/controllers/users/users_controller.go
+++ go-microservices/bookstore_users-api/src/controllers/users/users_controller.go
@@ -47,3 +47,30 @@ func GetUser(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, result)
 }
+
+// UpdateUser api
+func UpdateUser(c *gin.Context) {
+	userID, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("user id should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	user.ID = userID
+
+	isPartial := c.Request.Method == http.MethodPatch
+
+	result, updateErr := services.UpdateUser(isPartial, user)
+	if updateErr != nil {
+		c.JSON(updateErr.Status, updateErr)
+		return
+	}
+	c.JSON(http.StatusOK, result)
+}
--- go-microservices/bookstore_users-api/src/domain/users/user_dao.go
+++ go-microservices/bookstore_users-api/src/domain/users/user_dao.go
@@ -10,6 +10,7 @@ import (
 const (
 	queryInsertUser = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?, ?, ?, ?);"
 	queryGetUser    = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id = ?"
+	queryUpdateUser = "UPDATE users SET first_name = ?, last_name = ?, email = ? WHERE id = ?;"
 )
 
 // Get user by id
@@ -49,5 +50,20 @@ func (user *User) Save() *errors.RestErr {
 	}
 	user.ID = userID
 
+	return nil
+}
+
+//Update user
+func (user *User) Update() *errors.RestErr {
+	stmt, err := users_db.Client.Prepare(queryUpdateUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(user.FirstName, user.LastName, user.Email, user.ID)
+	if err != nil {
+		return mysql_utils.ParseError(err)
+	}
 	return nil
 }
