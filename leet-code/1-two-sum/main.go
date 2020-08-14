399e0b5378c84ef1c1e32e7766a8011ec4d14298
Hunghh.DEV <hunghh.dev@gmail.com>
2020-08-15 01:04:41 +0700

    [leet-code] 1. two sum

---
+++ leet-code/1-two-sum/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+	var result []int
+	for i := 0; i < len(nums); i++ {
+		for j := len(nums) - 1; j >= 0; j-- {
+			if i == j {
+				continue
+			}
+			if target == (nums[j] + nums[i]) {
+				result = append(result, i, j)
+				return result
+			}
+		}
+	}
+	return result
+}
+
+func main() {
+	// nums := [...]int{2, 7, 11, 15}
+	// fmt.Println(twoSum(nums[:], 9))
+	nums := [...]int{3, 2, 4}
+	fmt.Println(twoSum(nums[:], 6))
+}
