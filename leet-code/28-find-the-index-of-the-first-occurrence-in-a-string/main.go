80e526023d16f1d205fd3dc4e2214e513619f7cb
hunghh.dev <hunghh.dev@gmail.com>
2025-02-01 23:26:50 +0700

    [leet-code] 28. Find the Index of the First Occurrence in a String(optimize memory)

--- leet-code/28-find-the-index-of-the-first-occurrence-in-a-string/main.go
+++ leet-code/28-find-the-index-of-the-first-occurrence-in-a-string/main.go
@@ -3,11 +3,26 @@ package main
 import "fmt"
 
 func strStr(haystack string, needle string) int {
+	if len(needle) == 0 {
+		return 0
+	}
+	if len(needle) > len(haystack) {
+		return -1
+	}
+
 	for i := 0; i <= len(haystack)-len(needle); i++ {
-		if haystack[i:i+len(needle)] == needle {
-			return i
+		if haystack[i] == needle[0] {
+			k := 1
+			for k < len(needle) && haystack[i+k] == needle[k] {
+				k++
+			}
+
+			if k == len(needle) {
+				return i
+			}
 		}
 	}
+
 	return -1
 }
 
