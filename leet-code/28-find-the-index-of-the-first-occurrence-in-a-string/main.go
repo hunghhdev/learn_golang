1970a84407b369f2a25f8ee6a876125e3060a07c
hunghh.dev <hunghh.dev@gmail.com>
2025-02-01 23:06:03 +0700

    [leet-code] 28. Find the Index of the First Occurrence in a String

---
+++ leet-code/28-find-the-index-of-the-first-occurrence-in-a-string/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func strStr(haystack string, needle string) int {
+	for i := 0; i <= len(haystack)-len(needle); i++ {
+		if haystack[i:i+len(needle)] == needle {
+			return i
+		}
+	}
+	return -1
+}
+
+func main() {
+	fmt.Println(strStr("hello", "ll"))
+	fmt.Println(strStr("aaaaa", "bba"))
+	fmt.Println(strStr("", ""))
+	fmt.Println(strStr("sadbutsad", "sad"))
+	fmt.Println(strStr("leetcode", "leeto"))
+}
